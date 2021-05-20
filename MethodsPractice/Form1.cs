7f70e219f8c62609a73d9d3414d80953319c39aa
MaeveWheaton <teamheaven10@outlook.com>
2021-05-20 09:45:25 -0400

    coded

--- MethodsPractice/Form1.cs
+++ MethodsPractice/Form1.cs
@@ -22,7 +22,10 @@ namespace MethodsPractice
             /// TODO - Place method calls here
 
             PrintSignature();
-
+            RectangleArea();
+            AddValues();
+            CircleArea();
+            CompareValues();
         }
 
         /// 1. Print a signature to the screen
@@ -39,7 +42,12 @@ namespace MethodsPractice
 
         public void RectangleArea()
         {
+            int width = 5;
+            int length = 7;
 
+            int area = width * length;
+
+            q2Output.Text = $" The area of a {width} x {length} rectangle is {area} units squared.";
         }
 
         /// 3. Create a method to get numbers from q3Num1Input and q3Num2Input,
@@ -47,6 +55,22 @@ namespace MethodsPractice
         /// 
         /// 5 + 4 = 9 
 
+        public void AddValues()
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(q3Num1Input.Text);
+                int num2 = Convert.ToInt32(q3Num2Input.Text);
+
+                int sum = num1 + num2;
+
+                q3Output.Text = $"{num1} + {num2} = {sum}";
+            }
+            catch
+            {
+                q3Output.Text = "Please enter integer";
+            }
+        }
 
         
         /// 4. Create a method to calculate the area of a circle based on  
@@ -54,7 +78,14 @@ namespace MethodsPractice
         /// 
         /// The area of a circle with radius of 4 is 50.24 units squared
 
+        public void CircleArea()
+        {
+            double radius = Convert.ToDouble(radiusInput.Text);
 
+            double area = Math.PI * Math.Pow(radius, 2);
+
+            q4Output.Text = $"The area of a circle with a radius of {radius} is {area.ToString(".##")} units squared.";
+        }
 
         /// 5. 3U Only - Create a method that will determine and display
         /// the maximum of 3 values entered into q5Num1Input, q5Num2Input,
@@ -63,6 +94,25 @@ namespace MethodsPractice
         /// Inputs: 5, 7, 2
         /// he largest value is 7
 
+        public void CompareValues()
+        {
+            double num1 = Convert.ToDouble(q5Num1Input.Text);
+            double num2 = Convert.ToDouble(q5Num2Input.Text);
+            double num3 = Convert.ToDouble(q5Num3Input.Text);
 
+            double max = num1;
+
+            if (num2 > max)
+            {
+                max = num2;
+            }
+            
+            if (num3 > max)
+            {
+                max = num3;
+            }
+            
+            q5Output.Text = $"The largest value is {max}";
+        }
     }
 }
